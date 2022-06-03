9b0cb9d0dfc2f1179e80e4e56c62aab1768c209a
mastercodercat <mastercoderoy64@gmail.com>
2022-06-03 13:49:13 -0400

    Added dropdown items, list schema

--- src/schema.ts
+++ src/schema.ts
@@ -5,6 +5,7 @@ import { Context } from './context'
 const typeDefs = `
 type Mutation {
   createForm(name: String!, form: String!): Form
+  createDropdownList(name: String!, items: [String!]): DropdownList
 }
 
 type Form {
@@ -15,8 +16,22 @@ type Form {
   form: String!
 }
 
+type DropdownItem {
+  id: Int!
+  name: String!
+}
+
+type DropdownList {
+  id: Int!
+  createdAt: DateTime!
+  updatedAt: DateTime!
+  name: String!
+  items: [DropdownItem]!
+}
+
 type Query {
   allForms: [Form!]!
+  allDropdownLists: [DropdownList!]!
 }
 
 scalar DateTime
@@ -26,6 +41,13 @@ const resolvers = {
   Query: {
     allForms: (_parent: any, _args: any, context: Context) => {
       return context.prisma.form.findMany()
+    },
+    allDropdownLists: (_parent: any, _args: any, context: Context) => {
+      return context.prisma.dropdownList.findMany({
+        include: {
+          items: true,
+        },
+      })
     },
   },
   Mutation: {
@@ -38,6 +60,25 @@ const resolvers = {
         data: {
           name: args.name,
           form: args.form,
+        },
+      })
+    },
+    createDropdownList: (
+      _parent: any,
+      args: { name: string; items: Array<string> },
+      context: Context,
+    ) => {
+      return context.prisma.dropdownList.create({
+        data: {
+          name: args.name,
+          items: {
+            create: args.items.map((item) => ({
+              name: item,
+            })),
+          },
+        },
+        include: {
+          items: true,
         },
       })
     },
